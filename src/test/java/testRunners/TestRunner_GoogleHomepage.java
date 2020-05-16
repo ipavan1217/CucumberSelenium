ac98ca7badb99513e28548ab97951bf4d90e2a98
Pavan Innamuri <Pavan_Innamuri@gap.com>
2020-05-16 12:13:36 -0700

    html reports

--- src/test/java/testRunners/TestRunner_GoogleHomepage.java
+++ src/test/java/testRunners/TestRunner_GoogleHomepage.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features", glue= "stepDefinitions")
+@CucumberOptions(features="src/test/resources/features", glue= "stepDefinitions",format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class TestRunner_GoogleHomepage {
 
 }
